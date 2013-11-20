ea3ead003c06476f7c68bacbfcd377f254589a7a
deanonishi <dean@rightscale.com>
2013-11-20 11:09:04 -0800

    Removed executable code

--- recipes/configure_app.rb
+++ recipes/configure_app.rb
@@ -1,12 +1,7 @@
 #
-# Cookbook Name:: app_tomcat
+# Cookbook Name:: acme-app
 #
 # Copyright RightScale, Inc. All rights reserved.
 # All access and use subject to the RightScale Terms of Service available at
 # http://www.rightscale.com/terms.php and, if applicable, other agreements
 # such as a RightScale Master Subscription Agreement.
-
-rightscale_marker
-
-version = "6"
-log "  Setting provider specific settings for tomcat"
--- recipes/install_app.rb
+++ recipes/install_app.rb
@@ -1,12 +1,8 @@
 #
-# Cookbook Name:: app_tomcat
+# Cookbook Name:: acme_app
 #
 # Copyright RightScale, Inc. All rights reserved.
 # All access and use subject to the RightScale Terms of Service available at
 # http://www.rightscale.com/terms.php and, if applicable, other agreements
 # such as a RightScale Master Subscription Agreement.
 
-rightscale_marker
-
-version = "6"
-log "  Setting provider specific settings for tomcat"
